e78313cb3dee26f7151df3ea95f0933b839db321
Brandon Holt <bholt@cs.washington.edu>
2013-08-12 11:47:38 -0700

    fix va_list stuff

--- print.hpp
+++ print.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
 template< size_t Size >
+inline void vsprintf_s(char (&buf)[Size], const char * fmt, va_list va) {
+  vsnprintf(buf, Size, fmt, va);
+}
+
+template< size_t Size >
 inline void sprintf_s(char (&buf)[Size], const char * fmt, ...) {
-  // va_list argptr;
-  // va_start(argptr, fmt);
-  // sprintf(buf, )
-  //
+  va_list argptr;
+  va_start(argptr, fmt);
+  vsprintf_s(buf, fmt, argptr);
+  va_end(argptr);
 }
 
 std::string str_fmt(const char* fmt, ...) {
-  char dest[1024 * 16];
+  char dest[1<<16];
   va_list argptr;
   va_start(argptr, fmt);
-  vsprintf(dest, fmt, argptr);
+  vsprintf_s(dest, fmt, argptr);
   va_end(argptr);
   return std::string(dest);
 }
