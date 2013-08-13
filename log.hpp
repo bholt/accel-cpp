abe14e214b607436d20ea1837d685f08b0434cf0
Brandon Holt <bholt@cs.washington.edu>
2013-08-13 15:59:00 -0700

    accel-cpp: disable checks if not compiled with debug

--- log.hpp
+++ log.hpp
@@ -18,6 +18,12 @@
 #define LLOG(to_print) LOG("@" << id << ": " << to_print)
 #define LDLOG(to_print) DLOG("@" << id << ": " << to_print)
 
-#define CHECK(bool_expr) if (!(bool_expr)) { LOG( "Check failed: "#bool_expr ); }
-#define CHECK_LT(a, b) if (!((a) < (b))) { LOG( "Check failed: " << (a) << " < " << (b) ); }
-#define CHECK_EQ(a, b) if (!((a) == (b))) { LOG( "Check failed: " << (a) << " == " << (b) ); }
+# if defined(DEBUG) || defined(_DEBUG)
+#   define CHECK(bool_expr) if (!(bool_expr)) { LOG( "Check failed: "#bool_expr ); }
+#   define CHECK_LT(a, b) if (!((a) < (b))) { LOG( "Check failed: " << (a) << " < " << (b) ); }
+#   define CHECK_EQ(a, b) if (!((a) == (b))) { LOG( "Check failed: " << (a) << " == " << (b) ); }
+# else
+#   define CHECK(bool_expr) do {} while (0)
+#   define CHECK_LT(a, b) do {} while (0)
+#   define CHECK_EQ(a, b) do {} while (0)
+# endif
