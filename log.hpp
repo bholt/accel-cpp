3e5a590178c923fa268e6454d52730bb75eb9e43
Brandon Holt <t-bholt@microsoft.com>
2013-08-14 16:21:16 -0700

    throw exception on CHECK failure

--- log.hpp
+++ log.hpp
@@ -18,10 +18,12 @@
 #define LLOG(to_print) LOG("@" << id << ": " << to_print)
 #define LDLOG(to_print) DLOG("@" << id << ": " << to_print)
 
+
+
 # if defined(DEBUG) || defined(_DEBUG)
-#   define CHECK(bool_expr) if (!(bool_expr)) { LOG( "Check failed: "#bool_expr ); }
-#   define CHECK_LT(a, b) if (!((a) < (b))) { LOG( "Check failed: " << (a) << " < " << (b) ); }
-#   define CHECK_EQ(a, b) if (!((a) == (b))) { LOG( "Check failed: " << (a) << " == " << (b) ); }
+#   define CHECK(bool_expr) if (!(bool_expr)) { LOG( "Check failed: "#bool_expr ); throw false; }
+#   define CHECK_LT(a, b) if (!((a) < (b))) { LOG( "Check failed: " << (a) << " < " << (b) ); throw false; }
+#   define CHECK_EQ(a, b) if (!((a) == (b))) { LOG( "Check failed: " << (a) << " == " << (b) ); throw false; }
 # else
 #   define CHECK(bool_expr) do {} while (0)
 #   define CHECK_LT(a, b) do {} while (0)
