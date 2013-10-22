f4e0a424da49b186dffa87572b5bb3be3e3527bb
Brandon Holt <bholt@cs.washington.edu>
2013-10-21 22:01:24 -0700

    add DCHECK*

--- log.hpp
+++ log.hpp
@@ -6,22 +6,29 @@
 
 #include <iostream>
 
+#ifdef _DEBUG
+#define DEBUG 1
+#endif
+
 #define LOG(to_print) \
     std::cerr << __FILE__ << ":" << __LINE__ << "] " << to_print << std::endl
 
-#if defined(DEBUG) || defined(_DEBUG)
+#ifdef DEBUG
 #define DLOG(to_print) LOG(to_print)
 #else
 #define DLOG(to_print) do {} while (0)
 #endif
 
+#define CHECK(bool_expr) if (!(bool_expr)) { LOG( "Check failed: "#bool_expr ); throw "Check failed."; }
+#define CHECK_LT(a, b) if (!((a) < (b))) { LOG( "Check failed: " << (a) << " < " << (b) ); throw "Check failed."; }
+#define CHECK_EQ(a, b) if (!((a) == (b))) { LOG( "Check failed: " << (a) << " == " << (b) ); throw "Check failed."; }
 
-# if defined(DEBUG) || defined(_DEBUG)
-#   define CHECK(bool_expr) if (!(bool_expr)) { LOG( "Check failed: "#bool_expr ); throw false; }
-#   define CHECK_LT(a, b) if (!((a) < (b))) { LOG( "Check failed: " << (a) << " < " << (b) ); throw false; }
-#   define CHECK_EQ(a, b) if (!((a) == (b))) { LOG( "Check failed: " << (a) << " == " << (b) ); throw false; }
-# else
-#   define CHECK(bool_expr) do {} while (0)
-#   define CHECK_LT(a, b) do {} while (0)
-#   define CHECK_EQ(a, b) do {} while (0)
-# endif
+#ifdef DEBUG
+#  define DCHECK(bool_expr) CHECK(bool_expr)
+#  define DCHECK_LT(a, b)   CHECK_LT(a, b)
+#  define DCHECK_EQ(a, b)   CHECK_EQ(a, b)
+#else
+#  define DCHECK(bool_expr) do {} while (0)
+#  define DCHECK_LT(a, b) do {} while (0)
+#  define DCHECK_EQ(a, b) do {} while (0)
+#endif
