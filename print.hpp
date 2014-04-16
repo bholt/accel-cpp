f7ad6a1a15bbcac2e471a5ad39861726e33b689a
Brandon Holt <bholt@cs.washington.edu>
2014-04-16 15:25:23 -0700

    add array_str function and overload << for vector to use it

--- print.hpp
+++ print.hpp
@@ -7,6 +7,8 @@
 #include <cstddef>
 #include <cstdarg>
 #include <string>
+#include <sstream>
+#include <vector>
 
 template< size_t Size >
 inline void vsprintf_s(char (&buf)[Size], const char * fmt, va_list va) {
@@ -36,4 +38,33 @@ std::string str_fmt(const char* fmt, ...) {
   vsprintf_s(dest, fmt, argptr);
   va_end(argptr);
   return std::string(dest);
+}
+
+template< int width = 10, typename ArrayT = std::nullptr_t >
+inline std::string array_str(const char * name, const ArrayT& array) {
+  bool multiline = array.size() > width;
+  std::stringstream ss;
+  if (name) {
+    ss << name << ": ";
+  }
+  ss << "[";
+  long i=0;
+  for (auto e : array) {
+    if (i % width == 0 && multiline) ss << "\n  ";
+    ss << " " << e;
+    i++;
+  }
+  ss << (multiline ? "\n" : " ") << "]";
+  return ss.str();
+}
+
+template< int width = 10, typename ArrayT = std::nullptr_t >
+inline std::string array_str(const ArrayT& array) {
+  return array_str(nullptr, array);
+}
+
+template< typename T >
+std::ostream& operator<<(std::ostream& o, std::vector<T> v) {
+  o << array_str(v);
+  return o;
 }
